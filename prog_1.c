862ee7fecaf43df9509b02092a9dd6c510db3c89
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-24 08:39:46 +0530

    area of Circle

--- prog_1.c
+++ prog_1.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+/*Write a function to calculate the area of a circle. 
+*(TSRS): Take Something Return Something
+*function: int areaOfCircle(int radius)
+*area = PI*radius*radius;
+***/
 
+float PI = 3.14;
+
+float areaOfCircle(int radius)
+{
+    return PI*radius*radius;
+}
+
+int main()
+{
+    int radius;
+    printf("Enter the radius of circle: ");
+    scanf("%d", &radius);
+    printf("Area of circle is: %f ", areaOfCircle(radius));
+    
+    return 0;
+}
